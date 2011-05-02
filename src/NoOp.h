1daccc9210509632525920737f37906ef441f2a9
Gary R. Van Sickle <grvs@users.sourceforge.net>
2011-05-02 03:24:33 +0000

    Added NoOp class.

---
+++ src/NoOp.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2011 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of CoFlo.
+ *
+ * CoFlo is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * CoFlo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * CoFlo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NOOP_H
+#define	NOOP_H
+
+#include "Statement.h"
+
+/**
+ * NoOp statement.  Used primarily to give basic blocks which have no actual Statements
+ * at least one Statement, so that graph manipulations can work while retaining these
+ * "empty" blocks.
+ */
+class NoOp : public Statement
+{
+public:
+	NoOp(Location *location);
+	NoOp(const NoOp& orig);
+	virtual ~NoOp();
+	
+	virtual std::string GetStatementText() const;
+
+private:
+
+};
+
+#endif	/* NOOP_H */
+
