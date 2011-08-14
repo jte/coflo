8535b2d1f2fbd47d784de1d0135e7d6485a67c4e
Gary R. Van Sickle <grvs@users.sourceforge.net>
2011-08-14 04:06:38 +0000

    Testsuite no longer generates tests at "make check" time which require autoconf.

--- test/test_source_file_2.c
+++ test/test_source_file_2.c
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright 2011 Gary R. Van Sickle (grvs@users.sourceforge.net).
  *
  * This file is part of CoFlo.
@@ -16,6 +16,7 @@
  */
 
 /**
+ * @file
  * This is the second file of the do-nothing sample C program used to test CoFlo.
  */
 
