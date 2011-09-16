fcb94c966ea848496c90a31ce34d571e5c4c6073
Gary R. Van Sickle <grvs@users.sourceforge.net>
2011-09-16 22:29:58 +0000

    Some new files.

---
+++ src/ResponseFileParser.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2011 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of CoFlo.
+ *
+ * CoFlo is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * CoFlo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * CoFlo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file */
+
+#ifndef RESPONSEFILEPARSER_H
+#define RESPONSEFILEPARSER_H
+
+#include <string>
+#include <vector>
+
+class ResponseFileParser
+{
+public:
+	ResponseFileParser();
+	virtual ~ResponseFileParser();
+
+	void Parse(const std::string &filename, std::vector<std::string> *args);
+};
+
+#endif /* RESPONSEFILEPARSER_H */
