0f474496e02cf83670332bb0bec9e7ff471b9328
grvs <grvs@64113e98-479c-48ba-a4cb-3c2dfe73dddc>
2011-10-23 23:12:15 +0000

    Refactoring of the analysis class hierarchy.

---
+++ src/controlflowgraph/CallStackFrameBase.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2011 Gary R. Van Sickle (grvs@users.sourceforge.net).
+ *
+ * This file is part of CoFlo.
+ *
+ * CoFlo is free software: you can redistribute it and/or modify it under the
+ * terms of version 3 of the GNU General Public License as published by the Free
+ * Software Foundation.
+ *
+ * CoFlo is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * CoFlo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "CallStackFrameBase.h"
+
+CallStackFrameBase::CallStackFrameBase(FunctionCallResolved *function_call_which_pushed_this_frame)
+{
+	m_function_call_which_pushed_this_frame = function_call_which_pushed_this_frame;
+}
+
+CallStackFrameBase::~CallStackFrameBase()
+{
+	// TODO Auto-generated destructor stub
+}
+
