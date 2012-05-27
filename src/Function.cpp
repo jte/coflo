7f188a66bc3af1444d1a4e5a2535763e7ce01c52
Gary R. Van Sickle <grvs@users.sourceforge.net>
2012-05-27 06:26:09 +0000

    Issue: #9 ---------- Bugtraq Message ---------- issue #9: Make RemainingInDegreeMap an instantiation of SparsePropertyMap. -------------------------------------
    More cleanup.  gtest build works and passes.

--- src/Function.cpp
+++ src/Function.cpp
@@ -48,6 +48,7 @@
 #include "controlflowgraph/edges/edge_types.h"
 #include "controlflowgraph/ControlFlowGraph.h"
 #include "controlflowgraph/algorithms/cfg_algs.h"
+#include "controlflowgraph/SparsePropertyMap.h"
 #include "controlflowgraph/algorithms/topological_visit_kahn.h"
 #include "controlflowgraph/visitors/ControlFlowGraphVisitorBase.h"
 //#include "controlflowgraph/ControlFlowGraphTraversalDFS.h"
