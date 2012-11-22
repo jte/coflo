cde229a7c23023f8955dde38b4538df412664852
grvs <grvs@64113e98-479c-48ba-a4cb-3c2dfe73dddc>
2012-11-22 21:19:09 +0000

    Issue #5: Added util/interceptor.sh.

---
+++ util/interceptor.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+# Copyright 2012 Gary R. Van Sickle (grvs@users.sourceforge.net).
+#
+# This file is part of CoFlo.
+#
+# CoFlo is free software: you can redistribute it and/or modify it under the
+# terms of version 3 of the GNU General Public License as published by the Free
+# Software Foundation.
+#
+# CoFlo is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# CoFlo.  If not, see <http://www.gnu.org/licenses/>.
+
+# Interceptor.sh: a short shell script for intecepting the command line parameters passed
+# to a program such as GCC.
+
+# We use environment variables here instead of command-line options so as to not complicate
+# the invocation of this script.
+if [ -z "${INTERCEPTOR_OUTPUT_FILE}" ];
+then
+	echo "${0}: error: no output file specified in env var \"${INTERCEPTOR_OUTPUT_FILE}\"";
+	return 1;
+fi
+
+echo "${@}" >> "${INTERCEPTOR_OUTPUT_FILE}"
+gcc $@
